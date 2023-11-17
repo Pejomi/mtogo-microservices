a4b032650d672d5fa17a5c00f89a9979b2fe08e5
MieThorberg <cph-mt373@cphbusiness.dk>
2023-11-17 13:03:13 +0100

    consumer-service setup

---
+++ consumer-service/src/test/java/dk/pejomi/consumerservice/ConsumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.pejomi.consumerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
