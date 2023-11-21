826d3ba0b7d245f8dd76230c12eae0adbb33b79d
MieThorberg <cph-mt373@cphbusiness.dk>
2023-11-21 14:03:44 +0100

    added mapstruct

--- consumer-service/src/main/java/dk/pejomi/consumerservice/service/impl/ConsumerServiceImpl.java
+++ consumer-service/src/main/java/dk/pejomi/consumerservice/service/impl/ConsumerServiceImpl.java
@@ -1,6 +1,7 @@
 package dk.pejomi.consumerservice.service.impl;
 
 import dk.pejomi.consumerservice.dto.ConsumerDTO;
+import dk.pejomi.consumerservice.mapper.ConsumerMapper;
 import dk.pejomi.consumerservice.model.Consumer;
 import dk.pejomi.consumerservice.repository.ConsumerRepository;
 import dk.pejomi.consumerservice.service.ConsumerService;
@@ -12,31 +13,11 @@ import org.springframework.stereotype.Service;
 public class ConsumerServiceImpl implements ConsumerService {
 
     private final ConsumerRepository consumerRepository;
+
     @Override
     public ConsumerDTO createConsumer(ConsumerDTO consumerDTO) {
         Consumer consumer = consumerRepository.save(
-                Consumer.builder()
-                        .username(consumerDTO.getUsername())
-                        .email(consumerDTO.getEmail())
-                        .phone(consumerDTO.getPhone())
-                        .street(consumerDTO.getStreet())
-                        .city(consumerDTO.getCity())
-                        .zipCode(consumerDTO.getZipCode())
-                        .country(consumerDTO.getCountry())
-                        .build()
-        );
-        return mapToConsumerDTO(consumer);
-    }
-
-    private ConsumerDTO mapToConsumerDTO(Consumer consumer) {
-        return ConsumerDTO.builder()
-                .username(consumer.getUsername())
-                .email(consumer.getEmail())
-                .phone(consumer.getPhone())
-                .street(consumer.getStreet())
-                .city(consumer.getCity())
-                .zipCode(consumer.getZipCode())
-                .country(consumer.getCountry())
-                .build();
+                ConsumerMapper.INSTANCE.toConsumer(consumerDTO));
+        return ConsumerMapper.INSTANCE.toConsumerDTO(consumer);
     }
 }
