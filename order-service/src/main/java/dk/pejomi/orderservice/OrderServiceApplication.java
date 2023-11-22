9ceb6fa7c2a7a1d872b249176c6bfe47307984ff
PeterNedergaard <78910318+PeterNedergaard@users.noreply.github.com>
2023-11-22 14:51:24 +0100

    Initial commit

---
+++ order-service/src/main/java/dk/pejomi/orderservice/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package dk.pejomi.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+
+}
---
+++ order-service/src/test/java/dk/pejomi/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.pejomi.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
