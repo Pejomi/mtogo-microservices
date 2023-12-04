e04b3f1e65184a40fa2af9baca355bc36c31da23
MieThorberg <cph-mt373@cphbusiness.dk>
2023-12-04 20:50:19 +0100

    allowing frontend to call order endpoint

--- order-service/src/main/java/dk/pejomi/orderservice/controller/OrderController.java
+++ order-service/src/main/java/dk/pejomi/orderservice/controller/OrderController.java
@@ -5,10 +5,7 @@ import dk.pejomi.orderservice.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +15,7 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
+    @CrossOrigin(origins = "http://localhost:3000")
     public ResponseEntity<OrderDto> placeOrder(@RequestBody OrderDto orderDto) {
 
         return new ResponseEntity<>(orderService.createOrder(orderDto), HttpStatus.CREATED);
