cfbd7300d09ce9ce63705f6edbd788a94e9b9a57
JonasRex <78900583+JonasRex@users.noreply.github.com>
2023-12-05 11:59:03 +0100

    updated

---
+++ consumer-service/src/test/java/dk/pejomi/consumerservice/integration/KafkaIntegrationTest.java
@@ -0,0 +1,47 @@
+package dk.pejomi.consumerservice.integration;
+
+import dk.pejomi.basedomain.event.OrderEvent;
+import dk.pejomi.consumerservice.ConsumerServiceApplication;
+import dk.pejomi.consumerservice.kafka.OrderConsumer;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+
+import dk.pejomi.orderservice.OrderServiceApplication;
+import dk.pejomi.orderservice.kafka.OrderProducer;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+@SpringBootTest(classes = {OrderServiceApplication.class, ConsumerServiceApplication.class})
+@DirtiesContext
+@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
+@ComponentScan(basePackages = "dk.pejomi.orderservice")
+
+
+@Disabled
+public class KafkaIntegrationTest {
+
+    @Autowired
+    private OrderConsumer consumer;
+    @Autowired
+    private OrderProducer producer;
+    @Test
+    public void should_receive_message_when_send_message() throws Exception {
+        // arrange
+        String message = "Hello World";
+        OrderEvent orderEvent = new OrderEvent();
+        orderEvent.setMessage(message);
+        // act
+        producer.sendMessage(orderEvent);
+        // assert
+        boolean messageConsumed = consumer.getLatch().await(10, TimeUnit.SECONDS);
+        assertTrue(messageConsumed);
+    }
+
+
+}
